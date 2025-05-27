d8a4e3dc822ddaa829e01fdb830199f7f3aef0ab
Adrian Lona <104098971+AdrianLonaFragoso@users.noreply.github.com>
2025-05-27 12:21:20 -0600

    lorem ipsum

--- api/main.py
+++ api/main.py
@@ -42,7 +42,7 @@ async def responder(request: Request):
             content={"detail": "Input contains forbidden SQL keywords or patterns."}
         )
 
-    response_text = input_data.text + " | Default response"
+    response_text = input_data.text + " | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     timestamp = datetime.utcnow().isoformat() + "Z"
 
     return {
